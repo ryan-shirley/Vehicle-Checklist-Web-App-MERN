7a600366395d212198d0847db84dc1fadc7befdc
Ryan Shirley <N00161198@student.iadt.ie>
2020-01-21 11:41:01 +0000

    Front End Clean Up

--- src/App.js
+++ src/App.js
@@ -1,59 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import './App.scss';
 
 // Pages
 import Home from './pages/Home'
-import List from './pages/List'
-import Coin from './pages/Coin'
 
-// Components
-import CryptoNav from './components/CryptoNav'
-
-// Context
-import { ThemeContext } from "./ThemeContext";
-
-class App extends React.Component {
-    constructor() {
-        super()
-
-        this.toggleTheme = () => {
-            this.setState(state => ({
-                style:
-                    state.style === 'dark'
-                        ? 'light'
-                        : 'dark',
-            }));
-          };
-
-        this.state = {
-            style: 'dark',
-            toggleTheme: this.toggleTheme
-        }
-    }
-
+class App extends Component {
     render() {
         return (
-            <ThemeContext.Provider value={this.state}>
-                <ThemeContext.Consumer>
-                    {({style, toggleTheme}) => (
-                        <main className={"bg-danger pb-4 theme-" + style}>
-                            <BrowserRouter>
-                                <CryptoNav />
-                                <div className={"bg-" + style  +" mx-4 mt-n3 position-relative py-5"} style={{zIndex:1}}>
-                                    <div className="container">
-                                        <Switch>
-                                            <Route exact path="/" component={Home} />
-                                            <Route path="/list" component={List} />
-                                            <Route path="/coin/:slug" component={Coin} />
-                                        </Switch>
-                                    </div>
-                                </div>
-                            </BrowserRouter>
-                        </main>
-                    )}
-                </ThemeContext.Consumer>
-            </ThemeContext.Provider>
+            <main>
+                <BrowserRouter>
+                    <div className="container">
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </main>
         );
     }
 }
--- src/ThemeContext.js
+++
@@ -1,8 +0,0 @@
-// ThemeContext.js
-// Manage the theme styles 'light' or 'dark'
-import React from "react";
- 
-export const ThemeContext = React.createContext({
-    style: 'dark',
-    toggleTheme: () => {}
-});
