b3a6f4d9e5ddfdd09e5b6020cee622b060a2341b
Ryan Shirley <N00161198@student.iadt.ie>
2020-01-27 11:33:13 +0000

    Basic React App Setup w/Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap'
 import './App.scss';
 
 // Pages
@@ -10,6 +11,16 @@ class App extends Component {
         return (
             <main>
                 <BrowserRouter>
+                    <Navbar bg="light" expand="lg">
+                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                        </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
                     <div className="container">
                         <Switch>
                             <Route exact path="/" component={Home} />
