e5583d83822519b673c79f9374edf7cf7e01e4c4
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-05 10:38:20 +0000

    Update records.js

--- backend/routes/records.js
+++ backend/routes/records.js
@@ -1,70 +1,76 @@
-const router = require('express').Router()
+const router = require("express").Router()
 
-const Record = require('../models/Record')
-const User = require('../models/User')
+const Record = require("../models/Record")
+const User = require("../models/User")
 
-const checkIfAuthenticated = require('../middleware/auth-middleware')
+const checkIfAuthenticated = require("../middleware/auth-middleware")
 
 /**
  * route('/').get() returns all records
  * for one user
  */
-router.route('/').get(checkIfAuthenticated, (req, res) => {
+router.route("/").get(checkIfAuthenticated, (req, res) => {
     // Change this for auth user when implemented
     const user_id = req.decoded._id
 
-    Record.find({ user_id: user_id})
-        .select('-user_id -checked_groups')
-        .sort('-date')
+    Record.find({ user_id: user_id })
+        .select("-user_id -checked_groups")
+        .sort("-date")
         .then(records => res.json(records))
-        .catch(err => res.status(400).json({
-            code: 400,
-            message: err.message
-        }))
+        .catch(err =>
+            res.status(400).json({
+                code: 400,
+                message: err.message
+            })
+        )
 })
 
 /**
  * route('/:id').get() returns single record
  */
-router.route('/:id').get(checkIfAuthenticated, (req, res) => {
+router.route("/:id").get(checkIfAuthenticated, (req, res) => {
     // Change this for auth user when implemented
     const user_id = req.decoded._id
     const id = req.params.id
 
-    Record.findOne({ _id: id})
-        .select('-user_id')
-        .populate('checked_groups.group_id')
+    Record.findOne({ _id: id })
+        .select("-user_id")
+        .populate("checked_groups.group_id")
         .then(records => res.json(records))
-        .catch(err => res.status(400).json({
-            code: 400,
-            message: err.message
-        }))
+        .catch(err =>
+            res.status(400).json({
+                code: 400,
+                message: err.message
+            })
+        )
 })
 
 /**
  * route('/').post() creates new record
  * for user
  */
-router.route('/').post(checkIfAuthenticated, async (req, res) => {
+router.route("/").post(checkIfAuthenticated, async (req, res) => {
     // Change this for auth user when implemented
     const user_id = req.decoded._id
     const record = req.body
 
-    if(!record) {
+    if (!record) {
         res.status(500).json({
             code: 500,
-            message: 'No record was provided'
+            message: "No record was provided"
         })
     }
-    
 
     try {
-        let user = await User.findOne({ _id: user_id }).select('vehicle.registration_number').populate('plant_id').exec()
+        let user = await User.findOne({ _id: user_id })
+            .select("vehicle.registration_number")
+            .populate("plant_id")
+            .exec()
         const registration_number = user.vehicle.registration_number
         const plant_name = user.plant_id.name
 
         // ********* TODO: Validate user trying to add to is same as logged in *********
-        
+
         // Add user, vehicle, plant and pass information (String as if driver plant or vehicle changes in future)
         record.user_id = user._id
         record.plant_name = plant_name
@@ -74,11 +80,9 @@ router.route('/').post(checkIfAuthenticated, async (req, res) => {
         record.passed = true
         for (var i = 0; i < record.checked_groups.length; i++) {
             for (var j = 0; j < record.checked_groups[i].checks.length; j++) {
-                console.log(record.checked_groups[i].checks[j].passed);
-                
                 if (!record.checked_groups[i].checks[j].passed) {
                     record.passed = false
-                    break;
+                    break
                 }
             }
         }
@@ -86,8 +90,7 @@ router.route('/').post(checkIfAuthenticated, async (req, res) => {
         const newRecord = new Record(record)
         const savedRecord = await newRecord.save()
         res.json(savedRecord)
-    }
-    catch (err) {
+    } catch (err) {
         res.status(400).json({
             code: 400,
             message: err.message
@@ -99,7 +102,7 @@ router.route('/').post(checkIfAuthenticated, async (req, res) => {
  * route('/:id').put() updates record
  * for user
  */
-router.route('/:id').put(checkIfAuthenticated, async (req, res) => {
+router.route("/:id").put(checkIfAuthenticated, async (req, res) => {
     // Change this for auth user when implemented
     const user_id = req.decoded._id
     const id = req.params.id
@@ -108,12 +111,13 @@ router.route('/:id').put(checkIfAuthenticated, async (req, res) => {
     // ********* TODO: Validate user trying to add to is same as logged in *********
 
     Record.findOneAndUpdate({ _id: id }, record, (err, newRecord) => {
-        if (err) return res.status(500).json({
-            code: 500,
-            message: err.message
-        })
+        if (err)
+            return res.status(500).json({
+                code: 500,
+                message: err.message
+            })
 
-        return res.send(newRecord);
+        return res.send(newRecord)
     })
 })
 
@@ -121,7 +125,7 @@ router.route('/:id').put(checkIfAuthenticated, async (req, res) => {
  * route('/:id').delete() deletes record
  * for user
  */
-router.route('/:id').delete(checkIfAuthenticated, async (req, res) => {
+router.route("/:id").delete(checkIfAuthenticated, async (req, res) => {
     // Change this for auth user when implemented
     const user_id = req.decoded._id
     const id = req.params.id
@@ -130,16 +134,17 @@ router.route('/:id').delete(checkIfAuthenticated, async (req, res) => {
     // ********* TODO: Validate user trying to add to is same as logged in *********
 
     Record.deleteOne({ _id: id }, (err, doc) => {
-        if (err) return res.status(500).json({
-            code: 500,
-            message: err.message
-        })
+        if (err)
+            return res.status(500).json({
+                code: 500,
+                message: err.message
+            })
 
         return res.json({
             code: 200,
             message: `Record with ID ${id} has been successfully deleted.`
         })
-    });
+    })
 })
 
-module.exports = router;
+module.exports = router
