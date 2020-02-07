527f3d7be7f05fafdccfdd5a7cc2cd525fec0aa6
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-07 20:32:41 +0000

    Login form + home page layout added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,26 @@
 import React from "react"
-import axios from 'axios'
-import { Form, Button, Row, Col, Alert } from "react-bootstrap"
+import axios from "axios"
+import { Form, Button, Row, Col, Alert, Container } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 class Login extends React.Component {
     constructor(props) {
         super(props)
 
+        let ndate = new Date()
+        let hours = ndate.getHours()
+        let message =
+            hours < 12
+                ? "Good Morning"
+                : hours < 18
+                ? "Good Afternoon"
+                : "Good Evening"
+
         this.state = {
             email: "",
             password: "",
-            error: ""
+            error: "",
+            welcomeMessage: message
         }
 
         // Binding this to work in the callback
@@ -23,13 +33,16 @@ class Login extends React.Component {
     componentDidMount() {
         let redirect_message = this.props.location.redirect_message
 
-        if(redirect_message) {
+        if (redirect_message) {
             this.setState({
                 error: redirect_message
             })
         }
     }
 
+    /**
+     * handleInputChange() Handle form input from user
+     */
     handleInputChange = e => {
         const target = e.target
         const { name, value } = target
@@ -39,6 +52,9 @@ class Login extends React.Component {
         })
     }
 
+    /**
+     * onSubmit() Submit form to login
+     */
     onSubmit = e => {
         e.preventDefault()
 
@@ -48,12 +64,15 @@ class Login extends React.Component {
         }
 
         axios
-            .post(process.env.REACT_APP_API_URI + '/login', user)
+            .post(process.env.REACT_APP_API_URI + "/login", user)
             .then(res => {
                 // save token in local storage
                 localStorage.setItem("jwtToken", res.data.token)
                 localStorage.setItem("UID", res.data.user._id)
-                localStorage.setItem("userFullName", res.data.user.first_name + ' ' + res.data.user.last_name)
+                localStorage.setItem(
+                    "userFullName",
+                    res.data.user.first_name + " " + res.data.user.last_name
+                )
 
                 this.props.onLogin(true)
                 this.props.history.push("/records")
@@ -67,56 +86,62 @@ class Login extends React.Component {
 
     render() {
         return (
-            <Form onSubmit={this.onSubmit} className="mt-5">
-                <Form.Group as={Row} controlId="formHorizontalIMDB">
-                    <Form.Label column sm={2}>
-                        Email
-                    </Form.Label>
-                    <Col sm={10}>
-                        <Form.Control
-                            type="email"
-                            placeholder="Email"
-                            name="email"
-                            value={this.state.email}
-                            onChange={this.handleInputChange}
-                            required
-                        />
+            <Container className="mt-5">
+                <div className="text-center">
+                    <h1>HGV Checklist System</h1>
+                    <p>
+                        {this.state.welcomeMessage} and welcome to my
+                        platform.
+                    </p>
+                    <hr style={{ width: 60 }} />
+                </div>
+
+                <Row className="justify-content-md-center mt-5">
+                    <Col sm={6}>
+                        <p className="text-center">Sign in with your credentials</p>
+
+                        <Form onSubmit={this.onSubmit} className="mt-3">
+                            {this.state.error && (
+                                <Alert variant="danger">
+                                    {this.state.error}
+                                </Alert>
+                            )}
+
+                            <Form.Group
+                                controlId="hgvLoginEmail"
+                            >
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Email"
+                                    name="email"
+                                    value={this.state.email}
+                                    onChange={this.handleInputChange}
+                                    required
+                                />
+                            </Form.Group>
+
+                            <Form.Group
+                                controlId="hgvLoginPassword"
+                            >
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Password"
+                                    name="password"
+                                    value={this.state.password}
+                                    onChange={this.handleInputChange}
+                                    required
+                                />
+                            </Form.Group>
+
+                            <Button type="submit">Sign In</Button>
+
+                            <Link to="/register" className="btn btn-link float-right">
+                                Register
+                            </Link>
+                        </Form>
                     </Col>
-                </Form.Group>
-
-                <Form.Group as={Row} controlId="formHorizontalTitle">
-                    <Form.Label column sm={2}>
-                        Password
-                    </Form.Label>
-                    <Col sm={10}>
-                        <Form.Control
-                            type="password"
-                            placeholder="Password"
-                            name="password"
-                            value={this.state.password}
-                            onChange={this.handleInputChange}
-                            required
-                        />
-                    </Col>
-                </Form.Group>
-
-                {this.state.error && (
-                    <Alert variant="danger">
-                        {this.state.error}
-                    </Alert>
-                )}
-
-                <Form.Group as={Row}>
-                    <Col sm={{ span: 10, offset: 2 }}>
-                        <Button type="submit">Login</Button>
-                    </Col>
-                </Form.Group>
-
-                <Link to="/register" className="btn btn-dark">
-                    Register
-                </Link>
-                
-            </Form>
+                </Row>
+            </Container>
         )
     }
 }
