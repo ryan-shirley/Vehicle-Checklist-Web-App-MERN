4a1155745f9d7bcd7bec3b266b930b5f97fc1239
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-07 20:39:01 +0000

    Register form layout fixed

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,6 @@
 import React from "react"
 import axios from "axios"
-import { Form, Button, Alert } from "react-bootstrap"
+import { Form, Button, Alert, Container, Row, Col } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 class Register extends React.Component {
@@ -130,128 +130,178 @@ class Register extends React.Component {
 
     render() {
         return (
-            <Form onSubmit={this.onSubmit}>
-                {this.state.error && (
-                    <Alert variant="danger">{this.state.error}</Alert>
-                )}
+            <Container className="mt-5 mb-5">
+                <div className="text-center">
+                    <h1>Register for an account</h1>
+                    <p>Use the awesome form below to request an account.</p>
+                    <hr style={{ width: 60 }} />
+                </div>
 
-                <Form.Group controlId="registerFirstName">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="firstName"
-                        value={this.state.firstName}
-                        onChange={this.handleInputChange}
-                        required
-                    />
-                </Form.Group>
-                <Form.Group controlId="registerLastName">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="lastName"
-                        value={this.state.lastName}
-                        onChange={this.handleInputChange}
-                        required
-                    />
-                </Form.Group>
-                <Form.Group controlId="registerRegistration_number">
-                    <Form.Label>Registration Number</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="registration_number"
-                        value={this.state.registration_number}
-                        onChange={this.handleInputChange}
-                        required
-                    />
-                </Form.Group>
-                <Form.Group controlId="registerMake">
-                    <Form.Label>Make</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="make"
-                        value={this.state.make}
-                        onChange={this.handleInputChange}
-                        required
-                    />
-                </Form.Group>
-                <Form.Group controlId="registerModel">
-                    <Form.Label>Model</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="model"
-                        value={this.state.model}
-                        onChange={this.handleInputChange}
-                        required
-                    />
-                </Form.Group>
+                <Row className="justify-content-md-center mt-5">
+                    <Col sm={6}>
+                        <p className="text-center">
+                            Please remember your credentials
+                        </p>
 
-                <Form.Group controlId="registerCheck_list_id">
-                    <Form.Label>Checklist</Form.Label>
-                    <Form.Control
-                        as="select"
-                        name="check_list_id"
-                        value={this.state.check_list_id}
-                        onChange={this.handleInputChange}
-                    >
-                        {this.state.checklists.map(list => (
-                            <option key={list._id} value={list._id}>
-                                {list.name}
-                            </option>
-                        ))}
-                    </Form.Control>
-                </Form.Group>
+                        <Form onSubmit={this.onSubmit} className="mt-3">
+                            {this.state.error && (
+                                <Alert variant="danger">
+                                    {this.state.error}
+                                </Alert>
+                            )}
 
-                <Form.Group controlId="registerPlant">
-                    <Form.Label>Plant</Form.Label>
-                    <Form.Control as="select">
-                        {this.state.plants.map(plant => (
-                            <option key={plant._id}>{plant.name}</option>
-                        ))}
-                    </Form.Control>
-                </Form.Group>
+                            <Row>
+                                <Form.Group
+                                    as={Col}
+                                    controlId="registerFirstName"
+                                >
+                                    <Form.Label>First Name</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="firstName"
+                                        value={this.state.firstName}
+                                        onChange={this.handleInputChange}
+                                        required
+                                    />
+                                </Form.Group>
+                                <Form.Group
+                                    as={Col}
+                                    controlId="registerLastName"
+                                >
+                                    <Form.Label>Last Name</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="lastName"
+                                        value={this.state.lastName}
+                                        onChange={this.handleInputChange}
+                                        required
+                                    />
+                                </Form.Group>
+                            </Row>
 
-                <Form.Group controlId="registerEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control
-                        type="email"
-                        name="email"
-                        value={this.state.email}
-                        onChange={this.handleInputChange}
-                        required
-                    />
-                </Form.Group>
+                            <Form.Group controlId="registerRegistration_number">
+                                <Form.Label>Registration Number</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="registration_number"
+                                    value={this.state.registration_number}
+                                    onChange={this.handleInputChange}
+                                    required
+                                />
+                            </Form.Group>
 
-                <Form.Group controlId="registerPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleInputChange}
-                        required
-                    />
-                </Form.Group>
+                            <Row>
+                                <Form.Group
+                                    as={Col}
+                                    controlId="registerMake"
+                                >
+                                    <Form.Label>Make</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="make"
+                                        value={this.state.make}
+                                        onChange={this.handleInputChange}
+                                        required
+                                    />
+                                </Form.Group>
+                                <Form.Group
+                                    as={Col}
+                                    controlId="registerModel"
+                                >
+                                    <Form.Label>Model</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="model"
+                                        value={this.state.model}
+                                        onChange={this.handleInputChange}
+                                        required
+                                    />
+                                </Form.Group>
+                            </Row>
 
-                <Form.Group controlId="registerConfirmPassword">
-                    <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        name="confirmPassword"
-                        value={this.state.confirmPassword}
-                        onChange={this.handleInputChange}
-                        required
-                    />
-                </Form.Group>
+                            <Row>
+                                <Form.Group
+                                    as={Col}
+                                    controlId="registerCheck_list_id"
+                                >
+                                    <Form.Label>Checklist</Form.Label>
+                                    <Form.Control
+                                        as="select"
+                                        name="check_list_id"
+                                        value={this.state.check_list_id}
+                                        onChange={this.handleInputChange}
+                                    >
+                                        {this.state.checklists.map(list => (
+                                            <option
+                                                key={list._id}
+                                                value={list._id}
+                                            >
+                                                {list.name}
+                                            </option>
+                                        ))}
+                                    </Form.Control>
+                                </Form.Group>
 
-                <Button variant="primary" type="submit">
-                    Register
-                </Button>
+                                <Form.Group
+                                    as={Col}
+                                    controlId="registerPlant"
+                                >
+                                    <Form.Label>Plant</Form.Label>
+                                    <Form.Control as="select">
+                                        {this.state.plants.map(plant => (
+                                            <option key={plant._id}>
+                                                {plant.name}
+                                            </option>
+                                        ))}
+                                    </Form.Control>
+                                </Form.Group>
+                            </Row>
 
-                <Link to="/" className="btn btn-dark">
-                    Login
-                </Link>
-            </Form>
+                            <Form.Group controlId="registerEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    name="email"
+                                    value={this.state.email}
+                                    onChange={this.handleInputChange}
+                                    required
+                                />
+                            </Form.Group>
+
+                            <Form.Group controlId="registerPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    name="password"
+                                    value={this.state.password}
+                                    onChange={this.handleInputChange}
+                                    required
+                                />
+                            </Form.Group>
+
+                            <Form.Group controlId="registerConfirmPassword">
+                                <Form.Label>Confirm Password</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    name="confirmPassword"
+                                    value={this.state.confirmPassword}
+                                    onChange={this.handleInputChange}
+                                    required
+                                />
+                            </Form.Group>
+
+                            <Button type="submit">Register</Button>
+
+                            <Link
+                                to="/"
+                                className="btn btn-link float-right"
+                            >
+                                Login
+                            </Link>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
