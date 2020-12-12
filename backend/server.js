a9b6b0eefe1fa563206fe4a6d3b0dbd5e1bc0998
Ryan Shirley <N00161198@student.iadt.ie>
2020-12-12 13:26:46 +0000

    Sentry monitoring Express

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 const express = require('express')
 const body_parser = require('body-parser')
 const app = express()
@@ -5,6 +7,29 @@ const port = process.env.PORT || 4000
 
 const cors = require('cors')
 require('dotenv').config()
+
+// Sentry Config
+Sentry.init({ 
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        // enable HTTP calls tracing
+        // new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ 
+            // to trace all requests to the default router
+            app, 
+            // alternatively, you can specify the routes you want to trace:
+            // router: someRouter, 
+        }),
+        new Tracing.Integrations.Mongo(),
+    ],
+    environment: process.env.NODE_ENV,
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+})
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 const mongoose = require('mongoose')
 const ATLAS_URI = process.env.ATLAS_URI
@@ -32,5 +57,16 @@ app.use('/users', usersRouter)
 app.use('/plants', plantsRouter)
 app.use('/check-lists', checkListRouter)
 app.use('/records', recordsRouter)
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+});  
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
